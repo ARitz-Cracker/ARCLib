e32e1a8918b32f5fb392141619d19b56c583c173
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2015-10-03 09:03:55 -0400

    Gotta make sure the addon has a language before we send one!
    Signed-off-by: Aritz Beobide-Cardinal <aritz@aritzcracker.ca>

--- lua/arclib/server/arcaddon_util.lua
+++ lua/arclib/server/arcaddon_util.lua
@@ -61,7 +61,7 @@ end
 
 util.AddNetworkString( "arclib_comm_lang" )
 function ARCLib.SendAddonLanguage(addon,v)
-	if !v["_"..addon.."_Lang_Place"] then
+	if istable(_G[addon].JSON_Lang) && !v["_"..addon.."_Lang_Place"] then
 		net.Start("arclib_comm_lang")
 		net.WriteInt(0,ARCBANK_ERRORBITRATE)
 		v["_"..addon.."_Lang_Place"] = 0
