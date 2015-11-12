d22a8976aeb853f430056d47b1229f6c2c52ae59
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2015-11-11 20:14:19 -0500

    Added something I would have added a long time ago,
    Signed-off-by: Aritz Beobide-Cardinal <aritz@aritzcracker.ca>

---
+++ lua/arclib/client/derma.lua
@@ -0,0 +1,71 @@
+function ARCLib.Derma_NumberRequest( strTitle, strText, numMin, numMax, numDefault, fnEnter, fnCancel, strButtonText, strButtonCancelText )
+	numMin = numMin or 0
+	numMax = numMax or 100
+	local Window = vgui.Create( "DFrame" )
+		Window:SetTitle( strTitle or "Message Title (First Parameter)" )
+		Window:SetDraggable( false )
+		Window:ShowCloseButton( false )
+		Window:SetBackgroundBlur( true )
+		Window:SetDrawOnTop( true )
+		
+	local InnerPanel = vgui.Create( "DPanel", Window )
+		InnerPanel:SetDrawBackground( false )
+	
+	local Text = vgui.Create( "DLabel", InnerPanel )
+		Text:SetText( strText or "Message Text (Second Parameter)" )
+		Text:SizeToContents()
+		Text:SetContentAlignment( 5 )
+		Text:SetTextColor( color_white )
+		
+	local TextEntry = vgui.Create( "Slider", InnerPanel )
+		TextEntry:SetMin(numMin)
+		TextEntry:SetMax(numMax)
+		TextEntry:SetDecimals( 0 )
+		TextEntry:SetValue( numDefault or ((numMin+numMax)/2) )
+	local ButtonPanel = vgui.Create( "DPanel", Window )
+		ButtonPanel:SetTall( 30 )
+		ButtonPanel:SetDrawBackground( false )
+		
+	local Button = vgui.Create( "DButton", ButtonPanel )
+		Button:SetText( strButtonText or "OK" )
+		Button:SizeToContents()
+		Button:SetTall( 20 )
+		Button:SetWide( Button:GetWide() + 20 )
+		Button:SetPos( 5, 5 )
+		Button.DoClick = function() Window:Close() fnEnter( TextEntry:GetValue() ) end
+		
+	local ButtonCancel = vgui.Create( "DButton", ButtonPanel )
+		ButtonCancel:SetText( strButtonCancelText or "Cancel" )
+		ButtonCancel:SizeToContents()
+		ButtonCancel:SetTall( 20 )
+		ButtonCancel:SetWide( Button:GetWide() + 20 )
+		ButtonCancel:SetPos( 5, 5 )
+		ButtonCancel.DoClick = function() Window:Close() if ( fnCancel ) then fnCancel( TextEntry:GetValue() ) end end
+		ButtonCancel:MoveRightOf( Button, 5 )
+		
+	ButtonPanel:SetWide( Button:GetWide() + 5 + ButtonCancel:GetWide() + 10 )
+	
+	local w, h = Text:GetSize()
+	w = math.max( w, 400 ) 
+	
+	Window:SetSize( w + 50, h + 25 + 75 + 10 )
+	Window:Center()
+	
+	InnerPanel:StretchToParent( 5, 25, 5, 45 )
+	
+	Text:StretchToParent( 5, 5, 5, 35 )	
+	
+	TextEntry:StretchToParent( 5, nil, 5, nil )
+	TextEntry:AlignBottom( 5 )
+	
+	--TextEntry:RequestFocus()
+	--TextEntry:SelectAllText( true )
+	
+	ButtonPanel:CenterHorizontal()
+	ButtonPanel:AlignBottom( 8 )
+	
+	Window:MakePopup()
+	Window:DoModal()
+	return Window
+
+end
