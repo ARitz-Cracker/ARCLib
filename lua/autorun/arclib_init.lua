5abc5f08eea8adc505a092d73c5ba2cebd1f98b9
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2017-06-21 08:16:30 -0400

    changed update date

--- lua/autorun/arclib_init.lua
+++ lua/autorun/arclib_init.lua
@@ -4,7 +4,7 @@ function ARCLib.Msg(msg)
 	Msg("ARCLib: "..tostring(msg).."\n")
 end
 ARCLib.Version = "1.7.4"
-ARCLib.Update = "May 26th 2017"
+ARCLib.Update = "June 20th 2017"
 ARCLib.Msg("ARitz Cracker's Libraries")
 ARCLib.Msg(table.Random({"My pile-o-spaghetti-code","This library assumes a lot about the addon it loads, doesn't it?","Maybe other people will find this useful.","[Insert witty message here]","These little messages are tradition!"}))
 ARCLib.Msg("Version: "..ARCLib.Version)
